34f13ea5142aa9658f012dbbddc7fe2efadee3ae
Gaëtan de Menten <gdementen@gmail.com>
2014-02-10 15:57:36 +0100

    s/Home/News and put About page before News

--- tinkerer/ext/metadata.py
+++ tinkerer/ext/metadata.py
@@ -141,8 +141,11 @@ def process_metadata(app, env):
                     env.blog_posts.append(doc)
                 elif env.blog_metadata[doc].is_page:
                     env.blog_pages.append(doc)
-
-    env.blog_page_list = [("index", UIStr.HOME)] + [(page, env.titles[page].astext()) for page in env.blog_pages]
+    pagelist = [("index", UIStr.HOME)] + [(page, env.titles[page].astext()) for page in env.blog_pages]
+    # put "about" page first
+    assert pagelist[1][0] == "pages/about"
+    pagelist.insert(0, pagelist.pop(1))
+    env.blog_page_list = pagelist
 
 
 
